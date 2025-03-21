fc670adaf40034dbdaa6d48a7098727bc7ea3e49
Jona Baez <contactoconingenium@gmail.com>
2025-03-21 10:03:07 -0600

    edicion en mainFlow

--- base-ts-meta-memory/src/templates/mainFlow.ts
+++ base-ts-meta-memory/src/templates/mainFlow.ts
@@ -3,7 +3,7 @@ import { opcionesFlow } from "./opcionesFlow";
 
 const mainFlow = addKeyword([EVENTS.WELCOME])
     .addAnswer("🙏 ¡Bendiciones! Hemos creado este chatbot para ayudarte a encontrar información sobre la Virgen de Guadalupe y fortalecer tu devoción. ")
-    .addAnswer("✨  Interactúa con nuestro chatbot y deja que la fe guíe tu camino. ¡La Virgen de Guadalupe te acompaña siempre!\n\n🌹 Aquí puedes: \n\n 📖 Conocer la historia y milagros de la Virgen de Guadalupe.\n\n🕊️ Aprender cómo rezar el Santo Rosario Guadalupano.\n\n📅 Recibir recordatorios de fechas importantes y festividades.\n\n🕯️ Pedir oraciones y encender una vela virtual.\n\n📜 Descargar materiales como oraciones y guías devocionales.")
+    .addAnswer("✨  Interactúa con nuestro chatbot y deja que la fe guíe tu camino. ¡La Virgen de Guadalupe te acompaña siempre!")
     .addAction( async (ctx, ctxFn) =>{
         return ctxFn.gotoFlow(opcionesFlow)
      })
