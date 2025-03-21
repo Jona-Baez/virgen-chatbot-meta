44bd6c69ba05dd0b8715c2633d2915737e080d32
Jona Baez <contactoconingenium@gmail.com>
2025-03-21 11:45:01 -0600

    edicion en mainFlow

--- base-ts-meta-memory/src/templates/list_templates/dosFlow.ts
+++ base-ts-meta-memory/src/templates/list_templates/dosFlow.ts
@@ -3,6 +3,6 @@ import { addKeyword } from "@builderbot/bot";
 const dosFlow= addKeyword("smartia119402")
     .addAnswer("Aprende paso a paso como Rezar el Rosario")
     .addAnswer("Te enviamos la guía GRATIS", {
-        media: "./assets/Como-rezar-el-rosario.pdf"
+        media: "./assets/como-rezar-el-rosario.pdf"
     })
 export { dosFlow };
