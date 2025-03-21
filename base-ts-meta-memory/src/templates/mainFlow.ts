32d8009b57f3a472fe4bbc5d759104e9fe94298c
Jona Baez <contactoconingenium@gmail.com>
2025-03-20 22:55:00 -0600

    edicion en mainFlow

--- base-ts-meta-memory/src/templates/mainFlow.ts
+++ base-ts-meta-memory/src/templates/mainFlow.ts
@@ -2,8 +2,8 @@ import { addKeyword, EVENTS } from "@builderbot/bot"
 import { opcionesFlow } from "./opcionesFlow";
 
 const mainFlow = addKeyword([EVENTS.WELCOME])
-    .addAnswer("👋 ¡Hola!, ¡Bienvenido a Chatbot de la Virgen de Guadalupe")
-    .addAnswer(" 🔥 Somos un restaurante tradicional que rescata los sabores de la cocina mexicana.\n\n👩‍🍳Desde tacos al pastor hasta deliciosos chiles en nogada, cada platillo está preparado con ingredientes frescos y recetas de la abuela.")
+    .addAnswer("👋 ¡Hola!, ¡Bienvenido al Chatbot de la Virgen de Guadalupe en Mexico")
+    .addAnswer(" 🙏 ¡Bendiciones! Hemos creado este chatbot para ayudarte a encontrar información sobre la Virgen de Guadalupe y fortalecer tu devoción. Aquí puedes: \n\n 📖 Conocer la historia y milagros de la Virgen de Guadalupe.\n\n🕊️ Aprender cómo rezar el Santo Rosario Guadalupano.\n\n📅 Recibir recordatorios de fechas importantes y festividades.\n\n🕯️ Pedir oraciones y encender una vela virtual.\n\n📜 Descargar materiales como oraciones y guías devocionales.\n\n✨ Interactúa con nuestro chatbot y deja que la fe guíe tu camino. ¡La Virgen de Guadalupe te acompaña siempre! 🌹")
     .addAction( async (ctx, ctxFn) =>{
         return ctxFn.gotoFlow(opcionesFlow)
      })
