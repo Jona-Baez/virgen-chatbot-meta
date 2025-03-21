cdf7924576ed1d5de573e751bd5c4564f7e8ca04
Jona Baez <contactoconingenium@gmail.com>
2025-03-21 17:48:06 -0600

    edicion en el numero de opciones

--- base-ts-meta-memory/src/templates/list_templates/dosFlow.ts
+++ base-ts-meta-memory/src/templates/list_templates/dosFlow.ts
@@ -2,7 +2,7 @@ import { addKeyword } from "@builderbot/bot";
 
 const dosFlow= addKeyword("smartia119402")
     .addAnswer("Aprende paso a paso como Rezar el Rosario")
-    .addAnswer("Te enviamos la guía GRATIS", {
+    .addAnswer("Te enviamos la Guía para Rezar el Santo Rosario *GRATIS*", {
         media: "./assets/comorezarelrosario.pdf"
     })
 export { dosFlow };
--- base-ts-meta-memory/src/templates/list_templates/unoFlow.ts
+++ base-ts-meta-memory/src/templates/list_templates/unoFlow.ts
@@ -1,7 +1,7 @@
 import { addKeyword, EVENTS } from "@builderbot/bot";
 
 const unoFlow = addKeyword("smartia119401")//Es la clave que invoca la opcion de la lista
-    .addAnswer("Te enviamos nuestra carta", {
+    .addAnswer("Te enviamos la Historia de la Virgen de Guadalupe en México *GRATIS*", {
         media: "./assets/historiadelavirgen.pdf"
     })
 
