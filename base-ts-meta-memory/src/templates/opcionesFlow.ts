249b5ca580e4ca9ae4cbb21080469d39fd023d1c
Jona Baez <contactoconingenium@gmail.com>
2025-03-20 23:53:51 -0600

    edicion en mainFlow

--- base-ts-meta-memory/src/templates/opcionesFlow.ts
+++ base-ts-meta-memory/src/templates/opcionesFlow.ts
@@ -27,47 +27,47 @@ const opcionesFlow = addKeyword('EVENTS.ACTION')
                                 {
                                     "id": "SM23022502",
                                     "title": "📍 Sucursales y horarios",//Limitado a 26 caracteres
-                                    "description": "1. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
+                                    "description": "2. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
                                 },
                                 {
                                     "id": "SM23022503",
                                     "title": "📅 Reservaciones",//Limitado a 26 caracteres
-                                    "description": "1. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
+                                    "description": "3. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
                                 },
                                 {
                                     "id": "SM23022504",
                                     "title": "🎉 Paquetes y eventos",//Limitado a 26 caracteres
-                                    "description": "1. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
+                                    "description": "4. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
                                 },
                                 {
                                     "id": "SM23022505",
                                     "title": "🎟️ Promociones y cupones",//Limitado a 26 caracteres
-                                    "description": "1. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
+                                    "description": "5. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
                                 },
                                 {
                                     "id": "SM23022506",
                                     "title": "📝 Quejas y sugerencias",//Limitado a 26 caracteres
-                                    "description": "1. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
+                                    "description": "6. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
                                 },
                                 {
                                     "id": "SM23022507",
                                     "title": "📋 Contratación",//Limitado a 26 caracteres
-                                    "description": "1. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
+                                    "description": "7. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
                                 },
                                 {
                                     "id": "SM23022508",
                                     "title": "💳 Facturación",//Limitado a 26 caracteres
-                                    "description": "1. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
+                                    "description": "8. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
                                 },
                                 {
                                     "id": "SM230225audio",
                                     "title": "🎵 Audio",//Limitado a 26 caracteres
-                                    "description": "1. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
+                                    "description": "9. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
                                 },
                                 {
                                     "id": "SM230225imagen",
                                     "title": "🎥 Imagen",//Limitado a 26 caracteres
-                                    "description": "1. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
+                                    "description": "10. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta"
                                 }
                             ]
                         }
