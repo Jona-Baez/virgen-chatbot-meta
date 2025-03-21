bc89468bc9191e619cb65e1107567e8bac06e93f
Jona Baez <contactoconingenium@gmail.com>
2025-03-21 15:16:50 -0600

    edicion en el numero de opciones

--- base-ts-meta-memory/src/templates/opcionesFlow.ts
+++ base-ts-meta-memory/src/templates/opcionesFlow.ts
@@ -22,16 +22,46 @@ const opcionesFlow = addKeyword('EVENTS.ACTION')
                                 {
                                     "id": "smartia119401",
                                     "title": "🍽️ Historia y milagros",//Limitado a 26 caracteres
-                                    "description": "*GRATIS* Conoce la historia y milagros de la Virgen de Guadalupe."//Limitado a 70 caracteres
+                                    "description": "1. Conocer la historia y milagros de la Virgen de Guadalupe."//Limitado a 70 caracteres
                                 },
                                 {
                                     "id": "smartia119402",
                                     "title": "📍 Santo Rosario",//Limitado a 26 caracteres
-                                    "description": "*GRATIS* Aprende cómo rezar el Santo Rosario."//Limitado a 70 caracteres
+                                    "description": "*GRATIS.* Aprender cómo rezar el Santo Rosario Guadalupano."//Limitado a 70 caracteres
                                 },
                                 {
                                     "id": "smartia119403",
-                                    "title": "Una oración para cada día del mes",//Limitado a 26 caracteres
+                                    "title": "",//Limitado a 26 caracteres
+                                    "description": ""//Limitado a 70 caracteres
+                                },
+                                {
+                                    "id": "smartia119404",
+                                    "title": "",//Limitado a 26 caracteres
+                                    "description": ""//Limitado a 70 caracteres
+                                },
+                                {
+                                    "id": "smartia119405",
+                                    "title": "",//Limitado a 26 caracteres
+                                    "description": ""//Limitado a 70 caracteres
+                                },
+                                {
+                                    "id": "smartia119406",
+                                    "title": "📝 Quejas y sugerencias",//Limitado a 26 caracteres
+                                    "description": "6. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta."//Limitado a 70 caracteres
+                                },
+                                {
+                                    "id": "smartia119407",
+                                    "title": "📋 Contratación",//Limitado a 26 caracteres
+                                    "description": "7. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta."//Limitado a 70 caracteres
+                                },
+                                {
+                                    "id": "smartia119408",
+                                    "title": "💳 Facturación",//Limitado a 26 caracteres
+                                    "description": "8. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta."//Limitado a 70 caracteres
+                                },
+                                {
+                                    "id": "SM230225audio",
+                                    "title": "🎵 Audio",//Limitado a 26 caracteres
                                     "description": "9. Descarga nuestra cartaDescarga nuestra cartaDescarga nuestra carta."//Limitado a 70 caracteres
                                 },
                                 {
