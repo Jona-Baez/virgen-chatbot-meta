6c8a408bdb5000980003e4b1c099cd633d835adb
Jona Baez <contactoconingenium@gmail.com>
2025-03-20 22:33:36 -0600

    edicion en mainFlow

--- base-ts-meta-memory/src/templates/mainFlow.ts
+++ base-ts-meta-memory/src/templates/mainFlow.ts
@@ -2,7 +2,7 @@ import { addKeyword, EVENTS } from "@builderbot/bot"
 import { opcionesFlow } from "./opcionesFlow";
 
 const mainFlow = addKeyword([EVENTS.WELCOME])
-    .addAnswer("👋 ¡Hola!, ¡Bienvenido a El Sazón de México!  🇲🇽 Disfruta de la mejor gastronomía mexicana con auténtico sabor casero")
+    .addAnswer("👋 ¡Hola!, ¡Bienvenido a Chatbot de la Virgen de Guadalupe")
     .addAnswer(" 🔥 Somos un restaurante tradicional que rescata los sabores de la cocina mexicana.\n\n👩‍🍳Desde tacos al pastor hasta deliciosos chiles en nogada, cada platillo está preparado con ingredientes frescos y recetas de la abuela.")
     .addAction( async (ctx, ctxFn) =>{
         return ctxFn.gotoFlow(opcionesFlow)
