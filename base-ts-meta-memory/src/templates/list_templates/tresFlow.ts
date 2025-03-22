b66fd8925da5dabc08d72e934031882815941def
Jona Baez <contactoconingenium@gmail.com>
2025-03-21 19:00:27 -0600

    edicion en el numero de opciones

--- base-ts-meta-memory/src/templates/list_templates/tresFlow.ts
+++ base-ts-meta-memory/src/templates/list_templates/tresFlow.ts
@@ -1,5 +1,5 @@
 import { addKeyword } from "@builderbot/bot";
 
 const tresFlow= addKeyword("smartia119403")
-    .addAnswer("🙏💙 Fortalece tu fe cada día \n\n'💙🙏Lleva contigo un devocionario especial con 30 plegarias y oraciones dedicadas a la Virgen de Guadalupe. 📖✨📿 Cada día, una oración diferente para pedir su protección, agradecer sus bendiciones y fortalecer tu devoción. \n\n🎁 BONO: Incluye una novena especial para momentos difíciles \n\n🔥 Oferta especial por tiempo limitado 🔥 \n\n💳 Adquiérelo hoy por solo $XX y descárgalo al instante. \n\n📥 ¡Haz clic aquí para obtener el tuyo! [Enlace de compra]")
+    .addAnswer("💙🙏Lleva contigo un devocionario especial con 30 plegarias y oraciones dedicadas a la Virgen de Guadalupe.\n\n📖✨📿 Cada día, una oración diferente para pedir su protección, agradecer sus bendiciones y fortalecer tu devoción. \n\n🎁 BONO: Incluye una novena especial para momentos difíciles. \n\n💳 Adquiérelo hoy por solo $XX y descárgalo al instante. \n\n💳 Realiza un deposito o transferencia a nombre Jonathan Baez a la tarjeta 4027 6658 8507 5698 de Banco Azteca\n\n📥Coloca en el concepto tu nombre para ubicar el pago\n\n📥Envía tu comprobante de pago al numero 5528659461 y recibe el Ebook directo a tu whastApp")
 export { tresFlow };
